8afe66f2e6b01e6c1c2f933b5f9c2f2d5c528f96
Rafaelly Rodrigues <rrafasrodrigues@gmail.com>
2023-05-02 20:06:01 -0300

    trains the model

--- conv_network.py
+++ conv_network.py
@@ -11,3 +11,29 @@ plt.imshow(train_images[IMG_INDEX], cmap='gray')
 plt.xlabel(class_names[train_labels[IMG_INDEX][0]])
 plt.show()
 
+#CNN architecture
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10)) # output (10 classes)
+
+# train
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+
+
+
+
+
+
